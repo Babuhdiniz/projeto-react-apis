c7c08a8f4a30385d495ea449d90fa35da8176229
Babuhdiniz <babuhdiniz@gmail.com>
2023-08-15 15:54:43 -0300

    Estrutura

---
+++ projeto-pokedex/src/Components/PokemonCard/PokemonCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from './pokemonCardStyle'
+
+const PokemonCard = () => {
+  return (
+    <div>
+      <h1>PokemonCard</h1>
+    </div>
+  )
+}
+
+export default PokemonCard
---
+++ projeto-pokedex/src/Pages/PokemonListPage/PokemonListPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PokemonListPage = () => {
+  return (
+    <div>
+      <h1>PokemonListPage</h1>
+    </div>
+  )
+}
+
+export default PokemonListPage
